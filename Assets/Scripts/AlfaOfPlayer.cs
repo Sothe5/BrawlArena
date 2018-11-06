728c308b5b92e3b3447237ca3c525f0b5457f92e
sothe5 <almaral4c@gmail.com>
2018-11-06 12:35:16 +0100

    Added all the initial project.

---
+++ Assets/Scripts/AlfaOfPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlfaOfPlayer : MonoBehaviour {
+
+private SpriteRenderer mySpriteRenderer;
+
+	void Start(){
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void turnAlfaUp (){
+		Color color = mySpriteRenderer.color;
+		color.a = 1;
+		mySpriteRenderer.color = color;
+
+	}
+
+
+	public void turnAlfaDown (){
+		Color color = mySpriteRenderer.color;
+		color.a = 0.7f;
+		mySpriteRenderer.color = color;
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [Range(1, 2)]
+    public int playerNumber;
+    public float speed;
+    public float jumpForce = 10;
+
+    private Rigidbody2D myRigidbody;
+    private Vector2 velocity;
+    private float remainTimeJump = 10;
+    private int jumpRemaining = 3;
+    private bool onGround = true;
+
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+
+        myRigidbody.velocity = velocity;
+        float horizontalDir = Input.GetAxisRaw("Horizontal" + playerNumber);
+
+        velocity.x = speed * horizontalDir;
+
+        remainTimeJump -= 1;
+
+        if (Input.GetKeyDown(KeyCode.W) && jumpRemaining > 0) {
+            velocity.y = jumpForce;
+            remainTimeJump = 10;
+            jumpRemaining--;
+            if (jumpRemaining <= 0) {
+                GetComponent<AlfaOfPlayer>().turnAlfaDown();
+            }
+        } else {
+            if (remainTimeJump <= 0 && velocity.y <= 0) {
+                velocity.y = 0;
+            } else {
+                velocity.y -= 1f;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S) && !onGround) {
+            myRigidbody.gravityScale = 60;
+        } else {
+            myRigidbody.gravityScale = 40;
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.CompareTag("Ground")) {
+            onGround = true;
+            jumpRemaining = 3;
+            GetComponent<AlfaOfPlayer>().turnAlfaUp();
+        }
+    }
+
+    // tiene pinta que el onground no se actualiza como debe
+    void OnCollisionExit2D(Collision2D col) {
+        if (col.gameObject.CompareTag("Ground")) {
+            onGround = false;
+        }
+    }
+}
